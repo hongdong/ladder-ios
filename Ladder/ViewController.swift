8e5613515c3b3312e6afba8de307837bbfbd6a7e
sheng <sheng@aofei.org>
2018-03-23 20:36:21 +0800

    feat: add basics

---
+++ Ladder/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Ladder
+//
+//  Created by Aofei Sheng on 2018/3/23.
+//  Copyright © 2018 Beijing Corestate Technology Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+	init() {
+		super.init(style: .grouped)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		navigationItem.title = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String
+		navigationController?.navigationBar.barStyle = .black
+		navigationController?.navigationBar.tintColor = .white
+		navigationController?.navigationBar.barTintColor = UIColor(red: 80 / 255, green: 140 / 255, blue: 240 / 255, alpha: 1)
+		navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+	}
+}
---
+++ PacketTunnel/PacketTunnelProvider.swift
@@ -0,0 +1,12 @@
+//
+//  PacketTunnelProvider.swift
+//  PacketTunnel
+//
+//  Created by Aofei Sheng on 2018/3/23.
+//  Copyright © 2018 Beijing Corestate Technology Co., Ltd. All rights reserved.
+//
+
+import NetworkExtension
+
+class PacketTunnelProvider: NEPacketTunnelProvider {
+}
